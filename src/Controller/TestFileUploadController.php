462fd3b8a6ae8832a70a749f94a83a179f1fe050
Gitolite Okaski <warzgibz@gmail.com>
2023-08-30 11:27:21 +0300

    Little sumthin

--- src/Controller/TestFileUploadController.php
+++ src/Controller/TestFileUploadController.php
@@ -12,6 +12,12 @@ class TestFileUploadController extends AbstractController
     #[Route('/file/upload', name: 'app_test_file_upload')]
     public function index(FilesystemOperator $defaultStorage): JsonResponse
     {
+        $precheck = $defaultStorage->fileExists('test.jpg');
+        $deleted = false;
+        if ($precheck) {
+            $deleted = true;
+            $defaultStorage->delete('test.jpg');
+        }
         $defaultStorage->write(
             'test.jpg',
             file_get_contents('https://picsum.photos/200/300.jpg')
@@ -20,6 +26,8 @@ class TestFileUploadController extends AbstractController
 
         return $this->json([
             'message' => 'Welcome to your new controller!',
+            'preCheck' => $precheck,
+            'deletedOld' => $deleted,
             'check' => $defaultStorage->fileExists('test.jpg'),
             'path' => 'src/Controller/TestFileUploadController.php',
         ]);
